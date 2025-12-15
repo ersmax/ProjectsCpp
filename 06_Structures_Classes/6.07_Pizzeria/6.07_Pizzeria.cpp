e1860ed4635b965ae60f853036e43fe1847fdbe4
ersmax <e.masarin@student.vu.nl>
2025-12-15 15:02:15 +0100

    Add display price

--- 06_Structures_Classes/6.07_Pizzeria/6.07_Pizzeria.cpp
+++ 06_Structures_Classes/6.07_Pizzeria/6.07_Pizzeria.cpp
@@ -1,18 +1,3 @@
-/*
-Define a class called Pizza that has member variables to track the type of pizza
-(either deep dish, hand tossed, or pan) along with the size (either small, medium,
-or large) and the number of pepperoni or cheese toppings. You can use constants to
-represent the type and size. Include mutator and accessor functions for your class.
-Create a void function, outputDescription( ), that outputs a textual description
-of the pizza object. Also include a function, computePrice( ), that computes the
-cost of the pizza and returns it as a double according to the following rules:
-Small pizza = $10 + $2 per topping
-Medium pizza = $14 + $2 per topping
-Large pizza = $17 + $2 per topping
-Write a suitable test program that creates and outputs a description and price of
-various pizza objects.
-*/
-
 #include <iostream>
 #include <string>
 #include <limits>
@@ -26,9 +11,13 @@ class Pizza
 {
 public:
 	void setType();
+	//   Postcondition: The type of pizza is set.
 	void setSize();
+	//   Postcondition: The size of pizza is set.
 	void setTopping();
+	//   Postcondition: set the number of pepperoni and cheese toppings.
 	void outputDescription();
+	//   Postcondition: A description of the pizza is output.
 private:
 	std::string type = PIZZA_TYPE[0];
 	std::string size = PIZZA_SIZE[0];
@@ -37,9 +26,14 @@ private:
 	std::string getType();
 	std::string getSize();
 	int getPepperoni() const;
+	//  Postcondition: The number of pepperoni toppings is returned.
 	int getCheese() const;
-	double computePice();
+	//  Postcondition: The number of cheese toppings is returned.
+	int computePrice();
+	//  Postcondition: The price of the pizza is computed and returned.
     int inputValidation(int min, int max);
+	//  Precondition: min <= max
+	//  Postcondition: returns a validated integer input between min and max
 };
 
 int main( )
@@ -115,6 +109,7 @@ void Pizza::outputDescription()
 	std::cout << getSize() << " Pizza " << getType()
 		<< " with " << getPepperoni() << " pepperoni"
 		<< " and " << getCheese() << " cheese\n";
+	std::cout << "Total price: $" << computePrice() << "\n";
 }
 
 std::string Pizza::getSize()
@@ -135,4 +130,14 @@ int Pizza::getPepperoni() const
 int Pizza::getCheese() const
 {
 	return cheese;
+}
+
+int Pizza::computePrice()
+{
+	int total = 0;
+	if (getSize() == PIZZA_SIZE[0]) total += 10;
+	else if (getSize() == PIZZA_SIZE[1]) total += 14;
+	else if (getSize() == PIZZA_SIZE[2]) total += 17;
+	total += 2 * (getPepperoni() + getCheese());
+	return total;
 }
