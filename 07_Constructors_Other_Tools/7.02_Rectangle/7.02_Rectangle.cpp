f6299852d136c5c6c6f81f90a96a1b3ab37dbdb7
admin <admin>
2026-01-26 13:43:23 +0100

    add documentation

--- 07_Constructors_Other_Tools/7.02_Rectangle/7.02_Rectangle.cpp
+++ 07_Constructors_Other_Tools/7.02_Rectangle/7.02_Rectangle.cpp
@@ -1,37 +1,26 @@
-/*
-Define a class called Rectangle that has two member variables of type double to
-represent the width and height of a rectangle. Include these member functions:
-a constructor to construct the Rectangle class using width and height as arguments, 
-a default constructor that initializes both width and height to 5, and a
-member function each to set width and height. Also include a member function
-that computes the area of the rectangle, and output functions to display the width,
-height, and area of the rectangle.
-*/
-
-
 #include <iostream>
 
 class Rectangle
 {
 public:
 	Rectangle() : height(5.0), width(5.0) {}
-	Rectangle(double widthLength, double heightLength); 
+	Rectangle(const double widthLength, const double heightLength) : height(0.0), width(0.0) 
+														 { setWidth(widthLength); setHeight(heightLength); }
 	void setWidth(double widthLength);
+	//  Precondition: widthLength is non-negative
 	void setHeight(double heightLength);
+	//  Precondition: heightLength is non-negative
 	double getHeight() const { return height; }
 	double getWidth() const { return width; }
+	double getArea() const { return height * width; }
+	void displaySides() const;
+	void displayArea() const;
 private:
 	static bool validate(const double length) { return (length >= 0); }
+	// Postcondition: return true if length is non-negative
 	double height;
 	double width;
 };
-
-Rectangle::Rectangle(const double widthLength, 
-					 const double heightLength) : height(0.0), width(0.0)
-{
-	setWidth(widthLength);
-	setHeight(heightLength);
-}
 
 void Rectangle::setWidth(const double widthLength)
 {
@@ -49,6 +38,17 @@ void Rectangle::setHeight(const double heightLength)
 		throw std::invalid_argument("Invalid height");
 }
 
+void Rectangle::displaySides() const
+{
+	std::cout << "Height of rectangle: " << height
+			  << ", width of rectangle: " << width << '\n';
+}
+
+void Rectangle::displayArea() const
+{
+	std::cout << "Area of rectangle: " << getArea() << '\n';
+}
+
 int main( )
 {
 	const Rectangle aRectangle;
@@ -58,8 +58,8 @@ int main( )
 	try
 	{
 		const Rectangle anotherRectangle(10,2);
-		std::cout << "Height: " << anotherRectangle.getHeight() << '\n'
-			      << "Width: " << anotherRectangle.getWidth() << '\n';
+		anotherRectangle.displaySides();
+		anotherRectangle.displayArea();
 	} catch (const std::invalid_argument& e)
 	{
 		std::cout << "Error creating rectangle: " << e.what() << '\n';
