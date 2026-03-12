45cb472fbf1d50add8e7a5b0823f626a91e26f8b
ersmax <masarin.eros@gmail.com>
2026-03-12 19:54:49 +0100

    add logic to insert new tv

--- 10_Pointers_Dynamic_arrays/10.08_Television/10.08_Television.cpp
+++ 10_Pointers_Dynamic_arrays/10.08_Television/10.08_Television.cpp
@@ -20,8 +20,19 @@ use the mutator methods to set the values accordingly.
 #include <iostream>
 #include <string>
 
+class Television;
+
 typedef std::string *StringPtr;
-typedef const std::string *ConstStringPtr;
+typedef Television *TvPtr;
+
+constexpr const char *MENU =	"a. Add a new tv model\n"
+								"b. Print all tv models\n"
+								"c. Enter a tv's name to change characteristics\n"
+								"d. Enter a tv to remove it\n"
+								"e. Exit\n"
+								"Enter an option:\n";
+constexpr int N_PORTS = 4;
+constexpr std::string PORTS[N_PORTS] = {"HDMI", "DISPLAY PORT", "USB A", "USB C"};
 
 class Television
 {
@@ -36,6 +47,7 @@ public:
 																}
 	Television(const std::string& name, const double inches, const ConstStringPtr connections, int size);
 	~Television() { delete [] connectivitySupport; }
+	friend std::istream& operator >>(std::istream& inputStream, Television& newTelevision);
 private:
 	std::string displayType;
 	double dimension;
@@ -45,8 +57,19 @@ private:
 
 };
 
+
+bool menu(TvPtr& tvList, int& size);
+char inputValidation();
+void addTelevision(TvPtr& tvList, int& size);
+std::string enterName(std::istream& inputStream);
+
 int main( )
 {
+	TvPtr tvList = nullptr;
+	int size = 0;
+	while (menu()) {};
+
+	delete [] tvList;
 	std::cout << '\n';
 	return 0;
 }
@@ -59,3 +82,100 @@ Television::Television(const std::string& name, const double inches, const Const
 	for (int idx = 0; idx < sizeConnectivity; idx++)
 		connectivitySupport[idx] = connections[idx];
 }
+
+bool menu(TvPtr& tvList, int& size)
+{
+	std::cout << MENU;
+	switch (char answer = inputValidation())
+	{
+		case 'a':
+			addTelevision(tvList, size);
+			break;
+		case 'b':
+			break;
+		case 'c':
+			break;
+		case 'd':
+			break;
+		case 'e':
+			return false;
+		default:
+			return false;
+	}
+	return true;
+}
+
+std::istream& operator >>(std::istream& inputStream, Television& newTelevision)
+{
+	const std::string newDisplayType = enterName(inputStream);
+	const double newDimension = enterNumber(inputStream);
+	StringPtr newPorts = enterPorts(inputStream);
+	// TODO
+}
+
+std::string enterName(std::istream& inputStream)
+{
+	std::string name;
+	while (true)
+	{
+		std::cout << "Enter name:\n";
+		if (!std::getline(inputStream, name))
+		{
+			std::cerr << "Not a valid model name\n";
+			inputStream.clear();
+			inputStream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		if (name.empty())
+		{
+			std::cout << "Model must have a name\n";
+			continue;
+		}
+		return name;
+	}
+}
+
+//std::string nameValidation()
+//{
+//	std::string model;
+//	while (true)
+//	{
+//		if (!std::getline(, model))
+//	}
+//}
+
+void addTelevision(TvPtr& tvList, int& size)
+{
+	const TvPtr temp = new Television[size + 1];
+	for (int idx = 0; idx < size; idx++)
+		temp[idx] = tvList[idx];
+	
+	Television newTelevision;
+	std::cout << "Enter new television.\n";
+	std::cin >> newTelevision;
+	temp[size] = newTelevision;
+	size++;
+	delete [] tvList;
+	tvList = temp;
+}
+
+char inputValidation()
+{
+	char answer;
+	while (true)
+	{
+		if (!(std::cin >> answer))
+		{
+			std::cout << "Not a valid choice\n";
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		char letter = std::tolower(answer);
+		if (letter >= 'a' && letter <= 'e')
+			return letter;
+		std::cout << "Not a valid choice. Retry\n";
+	}
+}
+
