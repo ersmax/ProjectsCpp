2968e5cf23c5007202d98b4574fedd37740678e9
ersmax <masarin.eros@gmail.com>
2026-03-12 22:33:05 +0100

    add copy and assignment op

--- 10_Pointers_Dynamic_arrays/10.08_Television/10.08_Television.cpp
+++ 10_Pointers_Dynamic_arrays/10.08_Television/10.08_Television.cpp
@@ -17,22 +17,26 @@ constructor, and ask the user if customization is required for any television. I
 use the mutator methods to set the values accordingly.
 */
 
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 class Television;
 
 typedef std::string *StringPtr;
+typedef const std::string *ConstStringPtr;
 typedef Television *TvPtr;
+typedef const Television *ConstTvPtr;
 
-constexpr const char *MENU =	"a. Add a new tv model\n"
-								"b. Print all tv models\n"
-								"c. Enter a tv's name to change characteristics\n"
-								"d. Enter a tv to remove it\n"
-								"e. Exit\n"
+const std::string MENU =		"a. Add a new tv model\n"
+								"b. Enter a standard model and a number of that item\n"
+								"c. Print all tv models\n"
+								"d. Enter a tv's name to change characteristics\n"
+								"e. Enter a tv to remove it\n"
+								"f. Exit\n"
 								"Enter an option:\n";
 constexpr int N_PORTS = 4;
-constexpr std::string PORTS[N_PORTS] = {"HDMI", "DISPLAY PORT", "USB A", "USB C"};
+const std::string PORTS[N_PORTS] = {"HDMI", "DISPLAY PORT", "USB A", "USB C"};
 
 class Television
 {
@@ -47,7 +51,10 @@ public:
 																}
 	Television(const std::string& name, const double inches, const ConstStringPtr connections, int size);
 	~Television() { delete [] connectivitySupport; }
+	Television(const Television& secondTv);
+	Television& operator =(const Television& secondTv);
 	friend std::istream& operator >>(std::istream& inputStream, Television& newTelevision);
+	friend std::ostream& operator <<(std::ostream& outputStream, const Television& myTelevision);
 private:
 	std::string displayType;
 	double dimension;
@@ -59,19 +66,60 @@ private:
 
 
 bool menu(TvPtr& tvList, int& size);
+char enterInput();
 char inputValidation();
 void addTelevision(TvPtr& tvList, int& size);
+void showTelevision(const TvPtr& tvList, int size);
+
 std::string enterName(std::istream& inputStream);
+double enterNumber(std::istream& inputStream);
+
+StringPtr enterPorts(std::istream& inputStream, int& sizeNewPorts);
+void printPorts();
+bool isDuplicate(const ConstStringPtr& newPorts, const int size, const std::string& port);
+bool addPort(StringPtr& newPorts, int& size, const std::string& port);
 
 int main( )
 {
 	TvPtr tvList = nullptr;
 	int size = 0;
-	while (menu()) {};
+	while (menu(tvList, size)) {};
 
 	delete [] tvList;
 	std::cout << '\n';
 	return 0;
+}
+
+Television& Television::operator =(const Television& secondTv)
+{
+	if (this == &secondTv)	return *this;
+	displayType = secondTv.displayType;
+	dimension = secondTv.dimension;
+	delete [] connectivitySupport;
+	sizeConnectivity = secondTv.sizeConnectivity;
+	if (sizeConnectivity == 0)
+		connectivitySupport = nullptr;
+	else
+	{
+		connectivitySupport = new std::string[sizeConnectivity];
+		for (int idx = 0; idx < sizeConnectivity; idx++)
+			connectivitySupport[idx] = secondTv.connectivitySupport[idx];
+	}
+	return *this;
+}
+
+Television::Television(const Television& secondTv) :	displayType(secondTv.displayType),
+														dimension(secondTv.dimension),
+														sizeConnectivity(secondTv.sizeConnectivity)
+{
+	if (sizeConnectivity == 0)
+		connectivitySupport = nullptr;
+	else
+	{
+		connectivitySupport = new std::string[sizeConnectivity];
+		for (int idx = 0; idx < sizeConnectivity; idx++)
+			connectivitySupport[idx] = secondTv.connectivitySupport[idx];
+	}
 }
 
 Television::Television(const std::string& name, const double inches, const ConstStringPtr connections, const int size) : displayType(name)
@@ -92,12 +140,18 @@ bool menu(TvPtr& tvList, int& size)
 			addTelevision(tvList, size);
 			break;
 		case 'b':
+			// TODO
 			break;
 		case 'c':
+			showTelevision(tvList, size);
 			break;
 		case 'd':
+			// TODO
 			break;
 		case 'e':
+			// TODO
+			break;
+		case 'f':
 			return false;
 		default:
 			return false;
@@ -105,12 +159,123 @@ bool menu(TvPtr& tvList, int& size)
 	return true;
 }
 
+void showTelevision(const TvPtr& tvList, const int size)
+{
+	for (int idx = 0; idx < size; idx++)
+		std::cout << tvList[idx] << '\n';
+}
+
+void addTelevision(TvPtr& tvList, int& size)
+{
+	const TvPtr temp = new Television[size + 1];
+	for (int idx = 0; idx < size; idx++)
+		temp[idx] = tvList[idx];
+
+	Television newTelevision;
+	std::cout << "Enter new television.\n";
+	std::cin >> newTelevision;
+	temp[size] = newTelevision;
+	size++;
+	delete[] tvList;
+	tvList = temp;
+}
+
+std::ostream& operator <<(std::ostream& outputStream, const Television& myTelevision)
+{
+	outputStream << std::setw(18) << "Type: " << myTelevision.displayType << '\n';
+	outputStream << std::setw(18) << "Dimension: " << myTelevision.dimension << '\n';
+	outputStream << std::setw(18) << "Connectivity: ";
+	for (int idx = 0; idx < myTelevision.sizeConnectivity; idx++)
+	{
+		outputStream << myTelevision.connectivitySupport[idx];
+		if (idx != myTelevision.sizeConnectivity - 1)
+			outputStream << ", ";
+		else
+			outputStream << "\n";
+	}
+	return outputStream;
+}
+
 std::istream& operator >>(std::istream& inputStream, Television& newTelevision)
 {
 	const std::string newDisplayType = enterName(inputStream);
 	const double newDimension = enterNumber(inputStream);
-	StringPtr newPorts = enterPorts(inputStream);
-	// TODO
+	int sizeNewPorts = 0;
+	const StringPtr newPorts = enterPorts(inputStream, sizeNewPorts);
+	newTelevision.displayType = newDisplayType;
+	newTelevision.dimension = newDimension;
+	
+	delete [] newTelevision.connectivitySupport;
+	newTelevision.connectivitySupport = newPorts;
+	newTelevision.sizeConnectivity = sizeNewPorts;
+	return inputStream;
+}
+
+StringPtr enterPorts(std::istream& inputStream, int& sizeNewPorts)
+{
+	StringPtr newPorts = nullptr;
+	char answer = 'y';
+	while (std::tolower(answer) == 'y')
+	{
+		printPorts();
+		std::string port = enterName(inputStream);
+		if (isDuplicate(newPorts, sizeNewPorts, port))	continue;
+
+		if (!addPort(newPorts, sizeNewPorts, port))
+			std::cout << "Invalid port. Please enter one of the listed ports as shown\n";
+		
+		if (sizeNewPorts == N_PORTS)	break;
+		
+		std::cout << "Add another port? (y/n)\n";
+		answer = enterInput();
+	}
+	return newPorts;
+}
+
+bool addPort(StringPtr& newPorts, int& size, const std::string& port)
+{
+	StringPtr temp = new std::string[size + 1];
+	for (int idx = 0; idx < size; idx++)
+		temp[idx] = newPorts[idx];
+
+	bool found = false;
+	for (int idx = 0; idx < N_PORTS; idx++)
+		if (port == PORTS[idx])
+		{
+			found = true;
+			temp[size] = port;
+			break;
+		}
+	if (!found)
+	{
+		delete [] temp;
+		return false;
+	}
+	size++;
+	delete [] newPorts;
+	newPorts = temp;
+	return true;
+}
+
+bool isDuplicate(const ConstStringPtr& newPorts, const int size, const std::string& port)
+{
+	bool duplicatePort = false;
+	for (int idx = 0; idx < size; idx++)
+		if (port == newPorts[idx])
+		{
+			std::cout << "Port already entered. Please enter a different port\n";
+			duplicatePort = true;
+			break;
+		}
+	return duplicatePort;
+}
+
+void printPorts()
+{
+	std::cout << "Enter a new port among these\n";
+	for (int idx = 0; idx < N_PORTS - 1; idx++)
+		std::cout << "\"" << PORTS[idx] << "\", ";
+	std::cout << "\"" << PORTS[N_PORTS - 1] << '\n';
 }
 
 std::string enterName(std::istream& inputStream)
@@ -135,31 +300,29 @@ std::string enterName(std::istream& inputStream)
 	}
 }
 
-//std::string nameValidation()
-//{
-//	std::string model;
-//	while (true)
-//	{
-//		if (!std::getline(, model))
-//	}
-//}
-
-void addTelevision(TvPtr& tvList, int& size)
+double enterNumber(std::istream& inputStream)
 {
-	const TvPtr temp = new Television[size + 1];
-	for (int idx = 0; idx < size; idx++)
-		temp[idx] = tvList[idx];
-	
-	Television newTelevision;
-	std::cout << "Enter new television.\n";
-	std::cin >> newTelevision;
-	temp[size] = newTelevision;
-	size++;
-	delete [] tvList;
-	tvList = temp;
+	double number;
+	while (true)
+	{
+		std::cout << "Enter display in inches:\n";
+		if (!(inputStream >> number))
+		{
+			std::cerr << "Not a valid number\n";
+			inputStream.clear();
+			inputStream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		if (number < 1.0)
+		{
+			std::cout << "At least number greater than or equal 1.0\n";
+			continue;
+		}
+		return number;
+	}
 }
 
-char inputValidation()
+char enterInput()
 {
 	char answer;
 	while (true)
@@ -173,6 +336,15 @@ char inputValidation()
 		}
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		char letter = std::tolower(answer);
+		return letter;
+	}
+}
+
+char inputValidation()
+{
+	while (true)
+	{
+		char letter = enterInput();
 		if (letter >= 'a' && letter <= 'e')
 			return letter;
 		std::cout << "Not a valid choice. Retry\n";
