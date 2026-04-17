8a26ffbbebc651163a35b2bb694ab64c0d5fe59c
ersmax <masarin.eros@gmail.com>
2026-04-17 11:48:38 +0200

    add game poker

--- 14_Inheritance/14.13_Poker/14_13_Application.cpp
+++ 14_Inheritance/14.13_Poker/14_13_Application.cpp
@@ -5,3 +5,25 @@ program. For an additional challenge, incorporate a betting component.
 
 Note of implementation: I decided to implement Texas hold'em poker
 */
+
+#include <iostream>
+#include "14_13_Game.h"
+
+int main( )
+{
+	using myNamespacePoker::Game;
+	using myNamespacePoker::Player;
+
+	const Player alex("Alex", 100);
+	const Player sara("Sara", 100);
+	const Player marc("Marc", 100);
+	std::vector<Player> players;
+	players.push_back(alex);
+	players.push_back(sara);
+	players.push_back(marc);
+	Game aGame(players);
+	aGame.play();
+
+	std::cout << '\n';
+	return 0;
+}
